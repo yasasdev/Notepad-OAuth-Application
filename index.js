2a7e2330cd796bf3c7ad352e838ff8fb9ec5cc5e
Yasas Lekamge <yasaslekamge@gmail.com>
2024-11-16 23:20:08 +0530

    Initial commit.

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+import env from "dotenv";
+
+const app = express();
+const port = 3000;
+const saltRounds = 10;
+env.config();
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
+db.connect();
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
