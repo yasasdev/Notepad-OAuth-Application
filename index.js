55115e9e52a74abc7217ea0441ee663a30aa89ff
Yasas Lekamge <yasaslekamge@gmail.com>
2024-11-21 22:42:30 +0530

    index file update

--- index.js
+++ index.js
@@ -73,6 +73,33 @@ app.post("/register", async (req, res) => {
     }
 });
 
+passport.use(
+    "google",
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/auth/google/notes",
+            userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+        },
+        async (accessToken, refreshToken, profile, cb) => {
+            try {
+                const result = await db.query("SELECT * FROM users WHERE email = $1", [profile.email]);
+                if(result.rows.length === 0){
+                    const newUser = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)",
+                        [profile.email, "Google"]
+                    );
+                    return cb(null, newUser.row[0]);
+                } else {
+                    return cb(null, result.rows[0]);
+                }
+            } catch (err) {
+                return cb(err);                
+            }
+        }
+    )
+);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
